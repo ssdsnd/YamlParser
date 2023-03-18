9a48957bfc64fb90108b889a41e05896b350df81
ssdsnd <sujay.deodhar@gmail.com>
2023-03-18 18:45:08 +0530

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,98 +1,106 @@
-#include <iostream>
-#include <fstream>
 #include "rapidjson/reader.h"
+#include "rapidjson/document.h"
 #include "rapidjson/writer.h"
 #include "rapidjson/stringbuffer.h"
 
 using namespace rapidjson;
 
-class MyHandler : public BaseReaderHandler<UTF8<>, MyHandler> {
+class MyHandler : public BaseReaderHandler<> {
 public:
-    MyHandler(const std::string& targetPath, std::ostream& os) : targetPath_(targetPath), writer_(sb_), os_(os) {}
-
-    bool StartObject() {
-        path_.push_back(kObjectType);
-        if (isTargetPath()) {
-            currentRecord_.Clear();
-        }
-        return true;
-    }
-
-    bool Key(const char* str, SizeType length, bool copy) {
-        if (isTargetPath()) {
-            currentKey_ = std::string(str, length);
-        }
-        return true;
-    }
-
-    bool String(const char* str, SizeType length, bool copy) {
-        if (isTargetPath()) {
-            currentRecord_.AddMember(Value(currentKey_.c_str(), currentKey_.size(), currentRecord_.GetAllocator()).Move(), Value(str, length, currentRecord_.GetAllocator()).Move(), currentRecord_.GetAllocator());
-        }
-        return true;
-    }
+    MyHandler(const std::string& path) : path(path), in_array(false), in_object(false), depth(0) {}
 
     bool StartArray() {
-        path_.push_back(kArrayType);
-        if (isTargetPath()) {
-            handlerStack_.push(new MyHandler(targetPath_, os_));
-            return handlerStack_.top()->StartArray();
+        if (depth == path.size() && path[depth - 1] == '/') {
+            in_array = true;
+            return true;
+        }
+        depth++;
+        return true;
+    }
+
+    bool EndArray(SizeType size) {
+        if (depth == path.size() && path[depth - 1] == '/') {
+            in_array = false;
+            data.push_back(arr);
+            arr.clear();
+        }
+        depth--;
+        return true;
+    }
+
+    bool StartObject() {
+        if (depth == path.size() && path[depth - 1] == '/') {
+            in_object = true;
+            return true;
+        }
+        depth++;
+        return true;
+    }
+
+    bool EndObject(SizeType size) {
+        if (depth == path.size() && path[depth - 1] == '/') {
+            in_object = false;
+        }
+        depth--;
+        return true;
+    }
+
+    bool Key(const Ch* str, SizeType length, bool copy) {
+        key = str;
+        return true;
+    }
+
+    bool String(const Ch* str, SizeType length, bool copy) {
+        if (in_object && depth == path.size()) {
+            obj[key] = std::string(str, length);
         }
         return true;
     }
 
-    bool EndArray(SizeType elementCount) {
-        if (isTargetPath()) {
-            bool success = handlerStack_.top()->EndArray(elementCount);
-            delete handlerStack_.top();
-            handlerStack_.pop();
-            return success;
+    bool Int(int i) {
+        if (in_object && depth == path.size()) {
+            obj[key] = i;
         }
         return true;
     }
 
-    bool EndObject(SizeType memberCount) {
-        path_.pop_back();
-        if (isTargetPath()) {
-            currentRecord_.Accept(writer_);
-            currentRecord_.Clear();
+    bool Bool(bool b) {
+        if (in_object && depth == path.size()) {
+            obj[key] = b;
         }
         return true;
+    }
+
+    bool Double(double d) {
+        if (in_object && depth == path.size()) {
+            obj[key] = d;
+        }
+        return true;
+    }
+
+    void clear() {
+        data.clear();
+        arr.clear();
+    }
+
+    std::vector<std::map<std::string, boost::variant<int, std::string, bool, double>>> getData() {
+        return data;
     }
 
 private:
-    bool isTargetPath() const {
-        if (path_.size() < targetPath_.size()) {
-            return false;
-        }
-        for (size_t i = 0; i < targetPath_.size(); ++i) {
-            if (path_[i] != targetPath_[i]) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    std::vector<int> path_;
-    Document currentRecord_;
-    std::string currentKey_;
-    std::string targetPath_;
-    StringBuffer sb_;
-    Writer<StringBuffer> writer_;
-    std::ostream& os_;
-    std::stack<MyHandler*> handlerStack_;
+    std::string path;
+    bool in_array;
+    bool in_object;
+    int depth;
+    std::string key;
+    std::map<std::string, boost::variant<int, std::string, bool, double>> obj;
+    std::vector<std::map<std::string, boost::variant<int, std::string, bool, double>>> arr;
+    std::vector<std::map<std::string, boost::variant<int, std::string, bool, double>>> data;
 };
 
 int main() {
-    // Open input and output files
-    std::ifstream ifs("input.json");
-    std::ofstream ofs("output.json");
+    // JSON input
+    const char* json = "{\"users\": [{\"name\": \"Alice\", \"age\": 25, \"married\": false}, {\"name\": \"Bob\", \"age\": 30, \"married\": true}], \"groups\": [{\"name\": \"Admins\", \"members\": [\"Alice\", \"Bob\"]}, {\"name\": \"Users\", \"members\": [\"Charlie\", \"David\"]}]}";
 
-    // Parse input JSON using SAX model and write filtered records to output JSON file
-    MyHandler handler("path.to.array", ofs);
-    Reader reader;
-    char buffer[4096];
-    while (ifs.read(buffer, sizeof(buffer))) {
-        reader.Parse(buffer, ifs.gcount(), handler);
-    }
-}
+    // Create a reader
+    Reader reader
